62505c4ef3aa1b869fd46250aaa3bb93ddf1bb9d
lanasofia93 <60903344+lanasofia93@users.noreply.github.com>
2023-02-12 11:14:20 -0300

    Delete mysite directory

--- mysite/Perfil/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms 
-from .models import Perfil
-class createForm(forms.ModelForm):
-    class Meta:
-        model= Perfil
-        fields = ('Nome', 'Sobrenome', 'Email', 'Data_nascimento', )
