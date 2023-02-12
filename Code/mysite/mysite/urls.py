9d7d5f8842d87d694a256fad24a172778339aeb6
natmoraes07 <61123867+natmoraes07@users.noreply.github.com>
2023-02-12 19:14:31 -0300

    Add files via upload

--- Code/mysite/mysite/urls.py
+++ Code/mysite/mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('livros/', include('Livro.urls')),
     path('perfil/', include('Perfil.urls')),
+    path('leituras/', include('Leitura.urls'))
 ]
