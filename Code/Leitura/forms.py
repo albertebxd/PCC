d5c4475d01bc6a2d8d66f70cbc75b8f1ab0ca052
natmoraes07 <61123867+natmoraes07@users.noreply.github.com>
2023-04-17 23:56:59 -0300

    Delete Code/Leitura directory

--- Code/Leitura/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import Leitura
-
-class createForm(forms.ModelForm):
-    class Meta:
-        model= Leitura
-        fields = ('Status', 'Data_inicio', 'Data_final', 'Avaliacao')
