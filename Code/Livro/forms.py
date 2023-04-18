fb2147771443737044df039f545a8e1f3b58dc9f
natmoraes07 <61123867+natmoraes07@users.noreply.github.com>
2023-04-17 23:57:10 -0300

    Delete Code/Livro directory

--- Code/Livro/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import Livro
-
-class createForm(forms.ModelForm):
-    class Meta:
-        model= Livro
-        fields = ('Titulo', 'Editora', 'Autor', 'Ano_lancamento', 'Paginas', 'ISBN', 'Categoria', 'Imagem')
