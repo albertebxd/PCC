ef9c404684fec4e4f446b075cc73a2ad982e7766
avssmichelle <124034951+avssmichelle@users.noreply.github.com>
2023-05-29 20:55:49 -0300

    Delete users directory

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-   # path('home/', views.home, name='home'),
-    path('cadastro/', views.cadastro.as_view(), name='cadastro'),
-    
-]
